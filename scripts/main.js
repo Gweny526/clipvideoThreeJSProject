88930082c82403b77aa57c491f2b392dd8e3c4f5
Gweny Dos Santos <g.dossantos@interface3.be>
2024-11-13 13:03:45 +0100

    add changing form and rotation

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,13 @@ let renderer, camera, scene;
 let cube = [];
 let cylinder = [];
 
+let controls = new function () { 
+    this.rotationSpeed = 0.02; 
+    this.cameraPositionZ = 0.02;
+    this.cameraPositionX = 0.02;
+    this.cameraPositionY = 0.02;
+
+    }; 
 
 //les variable dans les functions seront local
 function init(){
@@ -43,7 +50,8 @@ function init(){
     scene.add(camera); 
 
     //ajout d'un mesh (ici un cube)
-    const geometry = new THREE.BoxGeometry(1 , 3, 1);
+    // const geometry = new THREE.BoxGeometry(1 , 3, 1);
+    const geometry = new THREE.TorusGeometry( 3, 0.5, 50, 20); 
     const material = new THREE.MeshBasicMaterial({ color: 0x800020 });
     for(let i = 0; i < 128; i ++)
     {
@@ -60,6 +68,11 @@ function init(){
     //         scene.add(cylinder[i]);  
     //     }
 
+    let gui = new dat.GUI(); 
+    gui.add(controls, 'rotationSpeed', -1, 1);
+    gui.add(controls, 'cameraPositionZ', -500, 500);
+    gui.add(controls, 'cameraPositionX', -500, 500);
+    gui.add(controls, 'cameraPositionY', -500, 500);
 
     // Renderer
     renderer = new THREE.WebGLRenderer({
@@ -78,6 +91,7 @@ function render(){
     analyserNode.getByteFrequencyData(dataArray);
     console.log(dataArray[5]) 
 
+
     let elapsedTime = new Date() - startingDate; // date du moment moins le startingDate(quand on clique sur le bouton)
     console.log(elapsedTime);
     //check state
@@ -90,11 +104,19 @@ function render(){
                     cube[i].scale.y = 1 + dataArray[i]/35;
                      
                     cube[i].material.color.setHex(0x5b2c6f);
-
+                    cube[i].rotation.z +=controls.rotationSpeed+i/128;
                     
                 }
             // cube.scale.y = 1 + dataArray[6]/100;
             // camera.lookAt(scene.position);
+            camera.position.z = 7.372 + elapsedTime * 0.001;
+            camera.position.x = 0.02
+            camera.position.y = 0.02
+
+            camera.lookAt(scene.position);
+
+
+
             if(elapsedTime > 27000) currentState = 1;
             break;
         case 1:
@@ -104,12 +126,14 @@ function render(){
                 {
                     cube[i].scale.y = 1 + dataArray[i]/30;
                     cube[i].material.color.setHex(0xf8bbd0);
-
+                    cube[i].rotation.z +=controls.rotationSpeed+i/128;
+                    
                     
                 }
-            // camera.position.x = -20 + 20*Math.sin(elapsedTime)/10;
+                // camera.position.x = -20 + 20*Math.sin(elapsedTime)/10;
+            camera.position.z = -250
             camera.position.x = 250 * Math.cos(elapsedTime/2000);
-            camera.position.z = Math.abs(250 * Math.sin(elapsedTime/2000));
+            //camera.position.z = Math.abs(250 * Math.sin(elapsedTime/2000));
             camera.lookAt(scene.position);
             // camera.position.x += 0.1;
             if(elapsedTime > 30000) currentState = 2;
@@ -121,7 +145,7 @@ function render(){
                 {
                     cube[i].scale.y = 1 + dataArray[i]/25;
                     cube[i].material.color.setHex(0xf1c40f);
-
+                    cube[i].rotation.z +=controls.rotationSpeed+i/128;
 
                     
                 }
@@ -137,6 +161,8 @@ function render(){
             {
                     cube[i].scale.y = 1 + dataArray[i]/20;
                     cube[i].material.color.setHex(0xf44336);
+                    cube[i].rotation.z +=controls.rotationSpeed+i/128;
+
 
                     
                     
@@ -153,6 +179,8 @@ function render(){
                 {
                     cube[i].scale.y = 1 + dataArray[i]/15;
                     cube[i].material.color.setHex(0xFF00CC);
+                    cube[i].rotation.z +=controls.rotationSpeed+i/128;
+
 
                     
                 }
@@ -169,12 +197,14 @@ function render(){
                 {
                     cube[i].scale.y = 1 + dataArray[i]/10;
                     cube[i].material.color.setHex(0x800020);
-
-                    
+                    cube[i].rotation.z +=controls.rotationSpeed+i/128;                    
                 }
-            camera.position.x = -300 * Math.cos(elapsedTime/2000);
-            camera.position.z = Math.abs(-300 * Math.sin(elapsedTime/2000));
+            // camera.position.x = -300 * Math.cos(elapsedTime/2000);
+            // camera.position.z = Math.abs(-300 * Math.sin(elapsedTime/2000));
             // camera.position.x = -20 + 100*Math.sin(elapsedTime)/2;
+            camera.position.z = 245.80;
+            camera.position.x = -54.66;
+            camera.position.y = -198.56;
             camera.lookAt(scene.position);
             break;
     }
